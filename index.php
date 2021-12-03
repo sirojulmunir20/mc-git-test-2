b82658b70ff0c59e46d5b0a4f5da59aa2fc8c879
sirojulmunir20 <sirojulmunir616@gmail.com>
2021-12-03 10:17:05 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat perspiciatis cupiditate, qui similique adipisci illo autem laudantium quis nam rem quaerat nihil est assumenda repudiandae neque ad accusantium repellat earum?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
